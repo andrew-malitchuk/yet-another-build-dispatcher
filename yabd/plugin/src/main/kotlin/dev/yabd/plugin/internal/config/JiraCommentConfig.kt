8a4c009977f6bf2452d73f0f43aec46995ffa11f
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2024-02-21 00:44:31 +0200

    YABD-1 Add JiraCommentConfig

---
+++ yabd/plugin/src/main/kotlin/dev/yabd/plugin/internal/config/JiraCommentConfig.kt
@@ -0,0 +1,13 @@
+package dev.yabd.plugin.internal.config
+
+@Suppress("ForbiddenComment")
+open class JiraCommentConfig {
+    // TODO: regex for email
+    var email: String? = null
+    var token: String? = null
+    var filePath: String? = null
+    var jiraCloudInstance: String? = null
+    var ticket: String? = null
+    var tag: String? = null
+    var artifactName: String? = null
+}
