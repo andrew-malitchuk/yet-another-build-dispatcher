c6e360278c6188704e6aaed8272528148f4f00bf
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2024-02-28 20:00:47 +0200

    YABD-3 Do refactoring

--- yabd/plugin/src/main/kotlin/dev/yabd/plugin/internal/YabdPlugin.kt
+++ yabd/plugin/src/main/kotlin/dev/yabd/plugin/internal/YabdPlugin.kt
@@ -1,5 +1,6 @@
 package dev.yabd.plugin.internal
 
+import com.android.build.gradle.api.ApplicationVariant
 import com.android.build.gradle.internal.plugins.AppPlugin
 import dev.yabd.plugin.common.core.ext.capitalize
 import dev.yabd.plugin.internal.YabdExtension.Companion.yabdConfig
@@ -10,6 +11,7 @@ import dev.yabd.plugin.internal.tasks.slack.SlackUploadTask
 import dev.yabd.plugin.internal.tasks.telegram.TelegramUploadTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.TaskContainer
 
 class YabdPlugin : Plugin<Project> {
     @Suppress("LongMethod")
@@ -22,66 +24,98 @@ class YabdPlugin : Plugin<Project> {
 
                 androidExtension.applicationVariants.all { variant ->
                     project.tasks.apply {
-                        register(
-                            "$TELEGRAM_UPLOAD${variant.name.capitalize()}",
-                            TelegramUploadTask::class.java,
-                        ) {
-                            yabd.telegram.apply {
-                                tag = variant.name
-                                it.group = "telegramUpload"
-                                it.description = "Task for ${variant.name} variant"
-                                it.telegramConfig.set(this)
-                            }
-                        }
-
-                        register(
-                            "$JIRA_UPLOAD${variant.name.capitalize()}",
-                            JiraUploadTask::class.java,
-                        ) {
-                            yabd.jira.apply {
-                                tag = variant.name
-                                it.group = "jiraUpload"
-                                it.description = "Task for ${variant.name} variant"
-                                it.jiraConfig.set(this)
-                            }
-                        }
-
-                        register(
-                            "$JIRA_COMMENT${variant.name.capitalize()}",
-                            JiraAttachCommentTask::class.java,
-                        ) {
-                            yabd.jiraCommentConfig.apply {
-                                it.group = "jiraUpload"
-                                it.description = "Task for ${variant.name} variant"
-                                it.jiraCommentConfig.set(this)
-                            }
-                        }
-
-                        register(
-                            "$JIRA_ATTACH_BUILD${variant.name.capitalize()}",
-                            JiraAttachBuildTask::class.java,
-                        ) {
-                            yabd.jiraAttachBuildConfig.apply {
-                                tag = variant.name
-                                it.group = "jiraUpload"
-                                it.description = "Task for ${variant.name} variant"
-                                it.jiraAttachBuildConfig.set(this)
-                            }
-                        }
-
-                        register(
-                            "$SLACK_UPLOAD${variant.name.capitalize()}",
-                            SlackUploadTask::class.java,
-                        ) {
-                            yabd.slackConfig.apply {
-                                tag = variant.name
-                                it.group = "slackUpload"
-                                it.description = "Task for ${variant.name} variant"
-                                it.slackConfig.set(this)
-                            }
-                        }
+                        configureTelegramUpload(variant, yabd)
+                        configureJiraUpload(variant, yabd)
+                        configureJiraComment(variant, yabd)
+                        configureJiraAttackBuild(variant, yabd)
+                        configureSlackUpload(variant, yabd)
                     }
                 }
+            }
+        }
+    }
+
+
+    private fun TaskContainer.configureSlackUpload(
+        variant: ApplicationVariant,
+        yabd: YabdExtension
+    ) {
+        register(
+            "$SLACK_UPLOAD${variant.name.capitalize()}",
+            SlackUploadTask::class.java,
+        ) {
+            yabd.slackConfig.apply {
+                tag = variant.name
+                it.group = "slackUpload"
+                it.description = "Task for ${variant.name} variant"
+                it.slackConfig.set(this)
+            }
+        }
+    }
+
+    private fun TaskContainer.configureJiraAttackBuild(
+        variant: ApplicationVariant,
+        yabd: YabdExtension
+    ) {
+        register(
+            "$JIRA_ATTACH_BUILD${variant.name.capitalize()}",
+            JiraAttachBuildTask::class.java,
+        ) {
+            yabd.jiraAttachBuildConfig.apply {
+                tag = variant.name
+                it.group = "jiraUpload"
+                it.description = "Task for ${variant.name} variant"
+                it.jiraAttachBuildConfig.set(this)
+            }
+        }
+    }
+
+    private fun TaskContainer.configureJiraComment(
+        variant: ApplicationVariant,
+        yabd: YabdExtension
+    ) {
+        register(
+            "$JIRA_COMMENT${variant.name.capitalize()}",
+            JiraAttachCommentTask::class.java,
+        ) {
+            yabd.jiraCommentConfig.apply {
+                it.group = "jiraUpload"
+                it.description = "Task for ${variant.name} variant"
+                it.jiraCommentConfig.set(this)
+            }
+        }
+    }
+
+    private fun TaskContainer.configureJiraUpload(
+        variant: ApplicationVariant,
+        yabd: YabdExtension
+    ) {
+        register(
+            "$JIRA_UPLOAD${variant.name.capitalize()}",
+            JiraUploadTask::class.java,
+        ) {
+            yabd.jira.apply {
+                tag = variant.name
+                it.group = "jiraUpload"
+                it.description = "Task for ${variant.name} variant"
+                it.jiraConfig.set(this)
+            }
+        }
+    }
+
+    private fun TaskContainer.configureTelegramUpload(
+        variant: ApplicationVariant,
+        yabd: YabdExtension
+    ) {
+        register(
+            "$TELEGRAM_UPLOAD${variant.name.capitalize()}",
+            TelegramUploadTask::class.java,
+        ) {
+            yabd.telegram.apply {
+                tag = variant.name
+                it.group = "telegramUpload"
+                it.description = "Task for ${variant.name} variant"
+                it.telegramConfig.set(this)
             }
         }
     }
