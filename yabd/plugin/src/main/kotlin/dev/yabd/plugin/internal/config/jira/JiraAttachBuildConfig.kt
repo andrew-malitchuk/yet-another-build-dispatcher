5d41b0b91dcf5597d83b9bf88015b2bd5fc79f7f
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2024-03-12 21:06:53 +0200

    YABD-5 Documentation

--- yabd/plugin/src/main/kotlin/dev/yabd/plugin/internal/config/jira/JiraAttachBuildConfig.kt
+++ yabd/plugin/src/main/kotlin/dev/yabd/plugin/internal/config/jira/JiraAttachBuildConfig.kt
@@ -1,12 +1,39 @@
 package dev.yabd.plugin.internal.config.jira
 
 open class JiraAttachBuildConfig {
+    /**
+     * Email associated with the Jira account.
+     */
     var email: String? = null
+
+    /**
+     * Token for authentication with Jira.
+     */
     var token: String? = null
+
+    /**
+     * Defines the path to the build artifact.
+     */
     var filePath: String? = null
+
+    /**
+     * Your Jira cloud namespace, f.e. `example.atlassian.net`
+     */
     var jiraCloudInstance: String? = null
+
+    /**
+     * Ticket artifact, f.e. JIRA-0123
+     */
     var ticket: String? = null
     var comment: String? = null
+
+    /**
+     * BuildVariant, f.e. `stageDebug`, `debug`, `release` etc
+     */
     var tag: String? = null
+
+    /**
+     * Your custom name for uploaded artifact
+     */
     var artifactName: String? = null
 }
--- yabd/plugin/src/main/kotlin/dev/yabd/plugin/internal/config/slack/SlackConfig.kt
+++ yabd/plugin/src/main/kotlin/dev/yabd/plugin/internal/config/slack/SlackConfig.kt
@@ -1,9 +1,31 @@
 package dev.yabd.plugin.internal.config.slack
 
+/**
+ * Configuration class for Slack settings and artifact information.
+ */
 open class SlackConfig {
+    /**
+     * Slack channel where the artifact will be posted.
+     */
     var channel: String? = null
+
+    /**
+     * Token for authentication with Slack.
+     */
     var token: String? = null
+
+    /**
+     * Defines the path to the build artifact.
+     */
     var filePath: String? = null
+
+    /**
+     * Build variant, for example, `stageDebug`, `debug`, `release`, etc.
+     */
     var tag: String? = null
+
+    /**
+     * Custom name for the uploaded artifact.
+     */
     var artifactName: String? = null
 }
